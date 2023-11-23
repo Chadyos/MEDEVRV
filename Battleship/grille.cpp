e4acd5ee14c309d99f18dc9fe516a840f79a869c
Chadyos <foraetan@gmail.com>
2023-11-23 10:30:06 +0100

    Création de la classe grille

---
+++ Battleship/grille.cpp
@@ -0,0 +1,26 @@
+# pragma once 
+#include "grille.h"
+#include <string>
+#include <iostream>
+using namespace std;
+// constructeur de la grille 
+
+grille::grille(){
+			for (int i=0; i<10; i++){
+			for (int j=0; j<10; j++){
+				m_tab[i][j] = "O";
+			}
+		}
+    };
+
+void grille::afficher(){
+		// affichage de la grille avec des barre 
+		cout << "  1 2 3 4 5 6 7 8 9 10" << endl;
+		for (int i=0; i<10; i++){
+			cout << i+1 << " ";
+			for (int j=0; j<10; j++){
+				cout << m_tab[i][j] << " ";
+			}
+			cout << endl;
+		}
+}
