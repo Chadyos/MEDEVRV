bea5470f69f458337e4a35cf56ec25b023c0ec48
BryanH01 <92093032+BryanH01@users.noreply.github.com>
2023-11-30 15:38:36 +0100

    décalage différent pour affichage ligne 10

--- src/grille.cpp
+++ src/grille.cpp
@@ -16,9 +16,9 @@ Grille::~Grille(){};
 
 void Grille::afficher(bool hide){
 		// affichage de la grille avec des barre 
-		cout << "  1 2 3 4 5 6 7 8 9 10" << endl;
+		cout << "   1 2 3 4 5 6 7 8 9 10" << endl;
 		for (int i=0; i<10; i++){
-			cout << i+1 << "  ";
+			cout << i+1 << (i == 9 ? " " : "  ");
 			for (int j=0; j<10; j++){
 				if(!hide){cout << m_tab[i][j] << " ";}
 				else {
