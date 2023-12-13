2b009eb7d9f77187815c743ebf5cf16635d29f7d
Chadyos <foraetan@gmail.com>
2023-12-13 09:06:10 +0100

    Ajout de la documentation de la gclasse grille

--- include/grille.h
+++ include/grille.h
@@ -1,20 +1,64 @@
-#pragma once 
+#pragma once
 #include <string>
 #include <iostream>
 using namespace std;
 
-// Création de la classe grille
+/**
+ * @brief Classe représentant la grille de jeu.
+ *
+ */
 
 class Grille
 {
 public:
+	/**
+	 * @brief Constructeur par défaut de la grille.
+	 * Initialise une grille 10x10 où chaque case est marquée par "O".
+	 */
 	Grille();
+	/**
+	 * @brief Destructeur de la grille.
+	 */
 	~Grille();
-
+	/**
+	 * @brief Affiche la grille dans la console.
+	 *
+	 * @param hide Si true, cache les navires non touchés sur la grille.
+	 */
 	void afficher(bool hide);
+	/**
+	 * @brief Vérifie si une case contient un navire.
+	 *
+	 * @param x Coordonnée X de la case à vérifier.
+	 * @param y Coordonnée Y de la case à vérifier.
+	 * @return true Si un navire est présent dans la case spécifiée.
+	 * @return false Si la case spécifiée ne contient pas de navire.
+	 */
 	bool isNavire(int x, int y);
+
+	/**
+	 * @brief Vérifie si un navire dans une case spécifiée est touché.
+	 *
+	 * @param x Coordonnée X de la case à vérifier.
+	 * @param y Coordonnée Y de la case à vérifier.
+	 * @return true Si le navire dans la case spécifiée est touché.
+	 * @return false Si le navire dans la case spécifiée n'est pas touché.
+	 */
 	void setNavire(int x, int y);
+	/**
+	 * @brief Modifie l'état d'une case de la grille après un tir.
+	 * Marque la case comme "X" si le tir est dans l'eau et comme "T" si un navire est touché.
+	 *
+	 * @param x Coordonnée X de la case à modifier.
+	 * @param y Coordonnée Y de la case à modifier.
+	 */
 	bool isNavireTouche(int x, int y);
+	/**
+	 * @brief Place un navire dans une case spécifiée de la grille.
+	 *
+	 * @param x Coordonnée X de la case où placer le navire.
+	 * @param y Coordonnée Y de la case où placer le navire.
+	 */
 	void tir_modif_case(int x, int y);
 	string m_tab[10][10];
 };
--- src/grille.cpp
+++ src/grille.cpp
@@ -2,69 +2,88 @@
 #include <string>
 #include <iostream>
 using namespace std;
-// constructeur de la grille 
 
-Grille::Grille(){
-			for (int i=0; i<10; i++){
-			for (int j=0; j<10; j++){
-				m_tab[i][j] = "O";
-			}
+Grille::Grille()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			m_tab[i][j] = "O";
 		}
-    };
+	}
+};
 
 Grille::~Grille(){};
 
-void Grille::afficher(bool hide){
-		// affichage de la grille avec des barre 
-		cout << "   1 2 3 4 5 6 7 8 9 10" << endl;
-		for (int i=0; i<10; i++){
-			cout << i+1 << (i == 9 ? " " : "  ");
-			for (int j=0; j<10; j++){
-				if(!hide){cout << m_tab[i][j] << " ";}
-				else {
-					string value = m_tab[i][j];
-					if (value == "N"){value ="O";}
-					cout << value << " ";}
-				}
-				cout << endl;
+void Grille::afficher(bool hide)
+{
+	// affichage de la grille avec des barre
+	cout << "   1 2 3 4 5 6 7 8 9 10" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i + 1 << (i == 9 ? " " : "  ");
+		for (int j = 0; j < 10; j++)
+		{
+			if (!hide)
+			{
+				cout << m_tab[i][j] << " ";
 			}
-			cout << endl;
+			else
+			{
+				string value = m_tab[i][j];
+				if (value == "N")
+				{
+					value = "O";
+				}
+				cout << value << " ";
+			}
 		}
-
-
-bool Grille::isNavire(int x, int y){
-		// si il y a un navire on retourne true sinon false 
-		if (m_tab[x][y] == "O" || m_tab[x][y] == "X"){
-			return false;
-		}
-		else{
-			return true;
-		} 
-		}
-
-bool Grille::isNavireTouche(int x, int y){
-		// si il y a un navire on retourne true sinon false 
-		if (m_tab[x][y] == "N"){
-			return false;
-		}
-		else{
-			return true;
-		} 
-		}
-
-
-
-void Grille::tir_modif_case(int x, int y){
-		if (m_tab[x][y] == "O"){
-			m_tab[x][y] = "X";
-		}
-		else if(m_tab[x][y]=="N"){
-			m_tab[x][y] = "T";
-		}
+		cout << endl;
+	}
+	cout << endl;
 }
 
+bool Grille::isNavire(int x, int y)
+{
+	// si il y a un navire on retourne true sinon false
+	if (m_tab[x][y] == "O" || m_tab[x][y] == "X")
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
 
-void Grille::setNavire(int x, int y){
-		// on met un navire dans la case 
-		m_tab[x][y] = "N";
+bool Grille::isNavireTouche(int x, int y)
+{
+	// si il y a un navire on retourne true sinon false
+	if (m_tab[x][y] == "N")
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+void Grille::tir_modif_case(int x, int y)
+{
+	if (m_tab[x][y] == "O")
+	{
+		m_tab[x][y] = "X";
+	}
+	else if (m_tab[x][y] == "N")
+	{
+		m_tab[x][y] = "T";
+	}
+}
+
+void Grille::setNavire(int x, int y)
+{
+	// on met un navire dans la case
+	m_tab[x][y] = "N";
 };
