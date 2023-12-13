e43844afadbaaeff390858f3bcd2252a998253ee
Chadyos <foraetan@gmail.com>
2023-12-13 09:36:01 +0100

    Déplacement de la doc de jeu dans le .h

--- include/jeu.h
+++ include/jeu.h
@@ -2,15 +2,51 @@
 #include <iostream>
 using namespace std;
 
-class Jeu {
-private : 
+class Jeu
+{
+private:
     int gagnant;
-    int nbTours ; 
-public :
-    void tourDeJeu ();
-    void setNbTour (int n);
-    int getNbTour ();
-    void setGagnant (int n);
-    int getGagnant ();
-    int genererCoord ();
+    int nbTours;
+
+public:
+    /**
+     * @brief Méthode qui gère le déroulement d'un tour de jeu.
+     *
+     * Cette méthode permet de gérer les actions effectuées lors d'un tour de jeu.
+     * Elle demande le nombre de joueurs et le nombre de bateaux, initialise les joueurs,
+     * puis effectue les actions de chaque joueur tour à tour jusqu'à ce qu'un joueur gagne.
+     *
+     * @return void
+     */
+    void tourDeJeu();
+    /**
+     * @brief Définit le nombre de tours du jeu.
+     *
+     * @param n Le nombre de tours à définir.
+     */
+    void setNbTour(int n);
+    /**
+     * @brief Renvoie le nombre de tours du jeu.
+     *
+     * @return Le nombre de tours du jeu.
+     */
+    int getNbTour();
+    /**
+     * @brief Définit le gagnant du jeu.
+     *
+     * @param n Le numéro du gagnant.
+     */
+    void setGagnant(int n);
+    /**
+     * @brief Renvoie le gagnant du jeu.
+     *
+     * @return Le gagnant du jeu.
+     */
+    int getGagnant();
+    /**
+     * Génère une coordonnée aléatoire entre 0 et 9.
+     *
+     * @return La coordonnée générée.
+     */
+    int genererCoord();
 };
--- src/jeu.cpp
+++ src/jeu.cpp
@@ -4,126 +4,111 @@
 #include "grille.h"
 using namespace std;
 
-/**
- * @brief Méthode qui gère le déroulement d'un tour de jeu.
- * 
- * Cette méthode permet de gérer les actions effectuées lors d'un tour de jeu.
- * Elle demande le nombre de joueurs et le nombre de bateaux, initialise les joueurs,
- * puis effectue les actions de chaque joueur tour à tour jusqu'à ce qu'un joueur gagne.
- * 
- * @return void
- */
-void Jeu::tourDeJeu () {
+void Jeu::tourDeJeu()
+{
     srand(time(0));
     Grille grille;
     int nbJoueur, nbBateaux;
     cout << "rentrer le nombre de joueur" << endl;
     cin >> nbJoueur;
-    while (nbJoueur != 1 && nbJoueur !=2) {
+    while (nbJoueur != 1 && nbJoueur != 2)
+    {
         cout << "rentrer le bon nombre" << endl;
         cin >> nbJoueur;
     }
     cout << "rentrer le nombre de bateaux" << endl;
     cin >> nbBateaux;
-    while (nbBateaux > 5 || nbBateaux < 1) {
+    while (nbBateaux > 5 || nbBateaux < 1)
+    {
         cout << "rentrer le bon nombre de bateaux" << endl;
-        cin >> nbBateaux; 
+        cin >> nbBateaux;
     }
     Joueur joueur1, joueur2;
-    joueur1.init (nbBateaux);
-    if (nbJoueur == 2) {
-        joueur2.init (nbBateaux);
+    joueur1.init(nbBateaux);
+    if (nbJoueur == 2)
+    {
+        joueur2.init(nbBateaux);
     }
-    else { joueur2.initAuto (nbBateaux);}
+    else
+    {
+        joueur2.initAuto(nbBateaux);
+    }
     int x, y;
-    for (;;) {
-        int n = getNbTour ();
-        this -> setNbTour (n+1);
-        do {
+    for (;;)
+    {
+        int n = getNbTour();
+        this->setNbTour(n + 1);
+        do
+        {
             cout << "joueur 1, a toi de jouer" << endl;
             cout << "entre x" << endl;
             cin >> x;
             cout << "entre y" << endl;
             cin >> y;
-        } while (grille.m_tab[x-1][y-1] == "X" || grille.m_tab[x-1][y-1] == "T");
-        joueur2.estTouche (x-1, y-1);
+        } while (grille.m_tab[x - 1][y - 1] == "X" || grille.m_tab[x - 1][y - 1] == "T");
+        joueur2.estTouche(x - 1, y - 1);
         joueur2.afficheGrilleAdv();
-        if (joueur2.ff ()) {
-            this -> setGagnant (1);
+        if (joueur2.ff())
+        {
+            this->setGagnant(1);
             break;
         }
-        if (nbJoueur == 2) {
-            do {
+        if (nbJoueur == 2)
+        {
+            do
+            {
                 cout << "joueur 2, a toi de jouer" << endl;
                 cout << "entre x" << endl;
                 cin >> x;
                 cout << "entre y" << endl;
                 cin >> y;
-            } while (grille.m_tab[x-1][y-1] == "X" || grille.m_tab[x-1][y-1] == "T");
-            joueur1.estTouche (x-1, y-1);
+            } while (grille.m_tab[x - 1][y - 1] == "X" || grille.m_tab[x - 1][y - 1] == "T");
+            joueur1.estTouche(x - 1, y - 1);
             joueur2.afficheGrilleAdv();
-
         }
-               else {
-            do {
-                x=genererCoord ();  
-                y=genererCoord ();
+        else
+        {
+            do
+            {
+                x = genererCoord();
+                y = genererCoord();
             } while (grille.m_tab[x][y] == "X" || grille.m_tab[x][y] == "T");
-            joueur1.estTouche (x,y);
+            joueur1.estTouche(x, y);
             joueur1.afficheGrilleAdv();
         }
-        if (joueur1.ff ()) {
-            setGagnant (2);
+        if (joueur1.ff())
+        {
+            setGagnant(2);
             break;
         }
     }
-    cout << "Bravo au joueur " << this -> getGagnant () << " d'avoir gagne" <<endl; 
+    cout << "Bravo au joueur " << this->getGagnant() << " d'avoir gagne" << endl;
 }
 
-/**
- * @brief Définit le nombre de tours du jeu.
- * 
- * @param n Le nombre de tours à définir.
- */
-void Jeu::setNbTour (int n) {
+void Jeu::setNbTour(int n)
+{
     nbTours = n;
-    return ;
+    return;
 }
 
-/**
- * @brief Renvoie le nombre de tours du jeu.
- * 
- * @return Le nombre de tours du jeu.
- */
-int Jeu::getNbTour () { 
+int Jeu::getNbTour()
+{
     return nbTours;
 }
 
-/**
- * @brief Définit le gagnant du jeu.
- * 
- * @param n Le numéro du gagnant.
- */
-void Jeu::setGagnant (int n) {
+void Jeu::setGagnant(int n)
+{
     gagnant = n;
-    return ;
+    return;
 }
 
-/**
- * @brief Renvoie le gagnant du jeu.
- * 
- * @return Le gagnant du jeu.
- */
-int Jeu::getGagnant () { 
+int Jeu::getGagnant()
+{
     return gagnant;
 }
 
-/**
- * Génère une coordonnée aléatoire entre 0 et 9.
- * 
- * @return La coordonnée générée.
- */
-int Jeu::genererCoord () {
+int Jeu::genererCoord()
+{
     int coord = std::rand() % 10;
     return coord;
 }
